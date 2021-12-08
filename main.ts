e01c16cfa3124e7d2ebfb46a139d6a9b166288c1
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2021-12-08 11:31:55 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,26 @@
 control.onEvent(EventBusSource.MICROBIT_ID_IO_P0, EventBusValue.MICROBIT_PIN_EVT_PULSE_HI, function () {
-    let Distance: number[] = []
-    let Temps: number[] = []
     Fente += 1
-    Temps[0] = input.runningTimeMicros() / 1000000
-    Distance[1] = Fente / 20 * 9.6 / 100
-    serial.writeString("" + Temps.length + "," + (Temps.length - 1))
+    Distance.push(Fente / 20 * 9.6 / 100)
+    Temps.push(input.runningTimeMicros() / 1000000)
+    Variation_distance = Distance[Distance.length - 2] - Distance[Distance.length - 1]
+    Variation_temps = Temps[Temps.length - 2] - Temps[Temps.length - 1]
+    VItesse = Variation_distance / Variation_temps
+    serial.writeString("" + (VItesse))
     serial.writeLine("")
 })
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Diamond)
+    basic.pause(1000)
+    basic.showIcon(IconNames.SmallDiamond)
+    control.reset()
+})
+let VItesse = 0
+let Variation_temps = 0
+let Variation_distance = 0
 let Fente = 0
-let liste = [0, 1]
+let Temps: number[] = []
+let Distance: number[] = []
+basic.showIcon(IconNames.Yes)
+Distance = []
+Temps = []
 Fente = 0
